5b58b78006fd774d1caa012ec33913d03749b518
skyemin <1515054372@qq.com>
2019-10-09 14:53:45 +0800

    snowflake实现分布式id

--- web/src/test/java/com/wei/web/WebApplicationTests.java
+++ web/src/test/java/com/wei/web/WebApplicationTests.java
@@ -1,16 +1,67 @@
 package com.wei.web;
 
+import com.wei.web.snow.IdGenerator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApplicationTests {
 
+    private static Logger log = LoggerFactory.getLogger(WebApplicationTests.class);
+
+    @Autowired
+    private IdGenerator idGenerator;
+
     @Test
-    public void contextLoads() {
+    public void testBatchId() {
+        for (int i = 0; i < 100; i++) {
+            String batchId = idGenerator.batchId(1001, 100);
+            log.info("批次号: {}", batchId);
+        }
+    }
+
+    @Test
+    public void testSimpleUUID() {
+        for (int i = 0; i < 100; i++) {
+            String simpleUUID = idGenerator.simpleUUID();
+            log.info("simpleUUID: {}", simpleUUID);
+        }
+    }
+
+    @Test
+    public void testRandomUUID() {
+        for (int i = 0; i < 100; i++) {
+            String randomUUID = idGenerator.randomUUID();
+            log.info("randomUUID: {}", randomUUID);
+        }
+    }
+
+    @Test
+    public void testObjectID() {
+        for (int i = 0; i < 100; i++) {
+            String objectId = idGenerator.objectId();
+            log.info("objectId: {}", objectId);
+        }
+    }
+
+    @Test
+    public void testSnowflakeId() {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(() -> {
+                log.info("分布式 ID: {}", idGenerator.snowflakeId());
+            });
+        }
+        executorService.shutdown();
     }
 
 }
